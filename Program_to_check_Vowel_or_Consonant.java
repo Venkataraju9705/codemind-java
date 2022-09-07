ddf5d8910580424a3b2ccdc93d805be4c1dbaf04
Venkataraju9705 <84895671+Venkataraju9705@users.noreply.github.com>
2022-09-07 07:47:00 -0700

    import java.util.Scanner; class vowel {  public static void main(String args[])  {    char ch;    Scanner sc=new Scanner(System.in);    ch=sc.nextLine().charAt(0);     if(ch=='A'||ch=='a')     {        System.out.println("Vowel");     }     else if(ch=='E'||ch=='e')     {        System.out.println("Vowel");       }        else if(ch=='I'||ch=='i')       {          System.out.println("Vowel");      }         else if(ch=='O'||ch=='o')     {         System.out.println("Vowel");        }          else if(ch=='U'||ch=='u')       {            System.out.println("Vowel");     }       else      {           System.out.println("Consonant");      }    } }

---
+++ Program_to_check_Vowel_or_Consonant.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+class vowel
+{
+ public static void main(String args[])
+ {
+   char ch;
+   Scanner sc=new Scanner(System.in);
+   ch=sc.nextLine().charAt(0);
+    if(ch=='A'||ch=='a')
+    {
+       System.out.println("Vowel");
+    }
+    else if(ch=='E'||ch=='e')
+    {
+       System.out.println("Vowel");
+      }
+       else if(ch=='I'||ch=='i')
+      {
+         System.out.println("Vowel");
+     }
+        else if(ch=='O'||ch=='o')
+    {
+        System.out.println("Vowel");
+       }
+         else if(ch=='U'||ch=='u')
+      {
+           System.out.println("Vowel");
+    }
+      else
+     {
+          System.out.println("Consonant");
+     }
+   }
+}
+          
+
+      
+       
