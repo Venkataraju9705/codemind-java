587e1f47107f44bd0b277d8a23ab38ba578a0e66
Venkataraju9705 <84895671+Venkataraju9705@users.noreply.github.com>
2022-09-07 07:59:20 -0700

    import java.util.Scanner; class PNnos {    public static void main(String args[])    {     int a;     Scanner sc=new Scanner(System.in);     a=sc.nextInt();      if (a>=0)        {          System.out.println("Positive Number");         }         else        {           System.out.println("Negative Number");        }       } }

---
+++ Program_to_check_whether_the_given_integer_is_positive_or_negative.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class PNnos
+{
+   public static void main(String args[])
+   {
+    int a;
+    Scanner sc=new Scanner(System.in);
+    a=sc.nextInt();
+     if (a>=0)
+       {
+         System.out.println("Positive Number");
+        }
+        else
+       {
+          System.out.println("Negative Number");
+       }
+      }
+}  
