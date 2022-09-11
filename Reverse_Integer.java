9850f8f9d7ee2df16be513bf41e55d25b2ed0d58
Venkataraju9705 <84895671+Venkataraju9705@users.noreply.github.com>
2022-09-11 13:35:27 -0200

    import java.util.Scanner; class reverse {   public static void main(String args[])   {   Scanner sc=new Scanner(System.in);    int n,r,sum=0;    n=sc.nextInt();    if(n>0) {    while(n>0) {    r=n%10;    sum=sum*10+r;    n=n/10; }   System.out.println(sum);  }   else  {    while(n<0)    {      r=n%10;      sum=sum*10+r;       n=n/10;    }   System.out.println(sum);   }
     } 
    }

---
+++ Reverse_Integer.java
@@ -0,0 +1,31 @@
+import java.util.Scanner; 
+class reverse
+{
+  public static void main(String args[])
+  {
+  Scanner sc=new Scanner(System.in);
+   int n,r,sum=0;
+   n=sc.nextInt();
+   if(n>0)
+{
+   while(n>0)
+{
+   r=n%10;
+   sum=sum*10+r;
+   n=n/10;
+}
+  System.out.println(sum);
+ }
+  else
+ {
+   while(n<0)
+   {
+     r=n%10;
+     sum=sum*10+r;
+      n=n/10;
+   }
+  System.out.println(sum);
+  }
+
+ } 
+}
