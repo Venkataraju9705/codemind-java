ea791ffd88644176e3e6d814eba4d711e7258cd7
Venkataraju9705 <84895671+Venkataraju9705@users.noreply.github.com>
2022-09-16 12:07:53 -0200

    import java.util.Scanner; class pandemic {    public static void main(String args[])     {       Scanner sc=new Scanner(System.in);       int n,b=0;       n=sc.nextInt();       if(n<3)       {         System.out.println(n-1);          }           else          System.out.println(2);      } }

---
+++ The_Pandemic.java
@@ -0,0 +1,16 @@
+import java.util.Scanner; 
+class pandemic
+{
+   public static void main(String args[])
+    {
+      Scanner sc=new Scanner(System.in);
+      int n,b=0;
+      n=sc.nextInt();
+      if(n<3)
+      {
+        System.out.println(n-1);
+         }
+          else
+         System.out.println(2);
+     }
+}
