ca5021bb9c2d0c9807cdeddba427dc44d8965d0f
Venkataraju9705 <84895671+Venkataraju9705@users.noreply.github.com>
2022-09-17 03:17:56 -0200

    import java.util.Scanner; class fib {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,a=0,b=1,c,found=0;         n=sc.nextInt();         c=a+b;         while(c<=n)         {             if(c==n)             {                 found=1;             }               c=a+b;               a=b;               b=c;         }          if(found==1)          System.out.println("True");          else          System.out.println("False");             }         }

---
+++ Fibonacci_or_not.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+class fib
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,a=0,b=1,c,found=0;
+        n=sc.nextInt();
+        c=a+b;
+        while(c<=n)
+        {
+            if(c==n)
+            {
+                found=1;
+            }
+              c=a+b;
+              a=b;
+              b=c;
+        }
+         if(found==1)
+         System.out.println("True");
+         else
+         System.out.println("False");
+            }
+        }
